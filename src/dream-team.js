6efd8211a2f010254b4a3f3883e22fa5462ba402
Sergey Kornienko <madkorney@gmail.com>
2022-03-12 18:18:32 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
+  if (!Array.isArray(members)) {return false}
+  if (members.lenfgth == 0) {return ''}
+
+  let teamName = members.reduce((prev, item, index)=>{
+    let curr = '';
+    if (typeof item == 'string') {
+      curr = item.trimStart().charAt(0).toUpperCase();
+    }
+    return prev + curr;
+  },'').split('').sort().join('');
+
+  return teamName;
+
+  }
+
+
+  // console.log(createDreamTeam([
+  //   '   William Alston ',
+  //   ' Paul Benacerraf',
+  //   '  Ross Cameron',
+  //   '       Gilles Deleuze',
+  //   '  Arda Denkel ',
+  //   '  Michael Devitt',
+  //   '  Kit Fine',
+  //   ' Nelson Goodman',
+  //   'David Kolb',
+  //   '   Saul Kripke',
+  //   '  Trenton Merricks',
+  //   '  Jay Rosenberg',
+  // ]) +' vs ' + 'ADGJKMNPRSTW');
+
+
+  // console.log(createDreamTeam([
+  //   ['David Abram'],
+  //   ['Robin Attfield'],
+  //   'Thomas Berry',
+  //   ['Paul R.Ehrlich'],
+  //   'donna Haraway',
+  //   ' BrIaN_gOodWiN  ',
+  //   {
+  //     0: 'Serenella Iovino'
+  //   },
+  //   'Erazim Kohak',
+  //   '  val_plumwood',
+  // ])+' vs ' +   'BDETV');
 
 module.exports = {
   createDreamTeam
