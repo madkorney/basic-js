7e8025ca6381e35ec7dcac8bf853b44af0fe8ca4
Sergey Kornienko <madkorney@gmail.com>
2022-03-13 15:15:46 +0300

    feat: add solution for hanoi-towers

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  //throw new NotImplementedError('Not implemented');
+  let hanoiSolution = {
+    'turns': Math.pow(2, disksNumber) - 1,
+    'seconds': Math.floor((Math.pow(2, disksNumber) - 1) / turnsSpeed * 3600),
+  }
+  
+  return hanoiSolution;
 }
+
+// console.log(calculateHanoi(5, 4074));
+// console.log(calculateHanoi(38, 4594));
+// console.log(calculateHanoi(34, 4005));
 
 module.exports = {
   calculateHanoi
 };
+
+
+// assert.deepEqual(calculateHanoi(5, 4074), { turns: 31, seconds: 27 });
+//             assert.deepEqual(calculateHanoi(38, 4594), { turns: 274877906943, seconds: 215402800390 });
+//             assert.deepEqual(calculateHanoi(34, 4005), { turns: 17179869183, seconds: 15442579040 });
+//             assert.deepEqual(calculateHanoi(19, 4770), { turns: 524287, seconds: 395688 });
+//             assert.deepEqual(calculateHanoi(23, 4344), { turns: 8388607, seconds: 6951884 });
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  //throw new NotImplementedError('Not implemented');
+  const SEASONSbyMONTHS = ['winter','winter','spring','spring','spring','summer','summer','summer','autumn','autumn','autumn','winter']; //autumn  = fall
+  const failure = 'Unable to determine the time of year!';
+  let season = ''; 
+
+  if (arguments.length == 0) {return failure}
+  if (Object.prototype.toString.call(date) === "[object Date]") {
+    if (isNaN(date)) { 
+      //invalid date
+      throw new Error('Invalid date!');
+    } else {
+      // date object is valid
+      return SEASONSbyMONTHS[date.getMonth()];
+    }
+  } else {
+    // not a date object
+    throw new Error('Invalid date!');
+  }
 }
+
+
+// try {
+// // console.log(getSeason(new Date(2361, 2, 24, 22, 13, 47, 69)));
+// let aaa = () => new Date();
+// console.log(aaa);
+// console.log(getSeason(aaa));
+// // console.log(getSeason('foo'));
+// // console.log(getSeason(['foo']));
+// // console.log(getSeason({john:'smith',}));
+// // console.log(getSeason(new Date('1990-11-28')));
+
+// console.log(getSeason(new Date('1990-13-28')));
+// }  catch(e) {
+//   console.log('поймал ');
+//   console.error(e);
+// }
+
+// () => getSeason('foo'),
+// () => getSeason({ John: 'Smith' }),
+// () => getSeason(20192701),
+// () => getSeason([2019, '27', 0 + '1']),
+// () => getSeason(() => new Date())
+
+
+
 
 module.exports = {
   getSeason
