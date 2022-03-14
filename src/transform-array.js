a9640f62dc74afd4f2ce8e3cbd89a0f7ffad2961
Sergey Kornienko <madkorney@gmail.com>
2022-03-14 12:16:06 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
+  if (!Array.isArray(arr)) {throw new Error(`'arr' parameter must be an instance of the Array!`)}
+  let transformedArr = [];
+  const CONTROL_COMMANDS = [`--discard-next`, `--discard-prev`,`--double-next`, `--double-prev`];
+
+  function isCommand(item) {
+    if (typeof item == 'string') {
+      if (CONTROL_COMMANDS.includes(item)) { 
+        return true 
+      } else {
+        return false}
+    } else {
+      return false
+    }
+  }
+  let discardNext = false;
+  let doubleNext = false;
+
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (isCommand(arr[i])) {
+      switch (arr[i]) {
+        case `--discard-next`:
+          discardNext = true;
+          break;
+        case `--double-next`:
+          doubleNext = true;
+          break;
+        case `--discard-prev`:
+          if (transformedArr.length > 0) {
+            if (transformedArr[transformedArr.length-1] == arr[i-1]) {
+              transformedArr.pop(); 
+            }
+          }
+          break;    
+        case `--double-prev`:
+          if (transformedArr.length > 0) {
+            if (transformedArr[transformedArr.length-1] == arr[i-1]) {
+              transformedArr.push(transformedArr[transformedArr.length-1]);
+            }
+          }
+          break;
+      }
+    } else {
+      if (discardNext) {
+        
+      } else if (doubleNext) {
+          
+          transformedArr.push(arr[i]);
+          transformedArr.push(arr[i]);
+        } else {
+            transformedArr.push(arr[i]);
+          }
+          discardNext = false; 
+          doubleNext = false;   
+    }
+
+  }
+
+
+
+  return transformedArr;
 }
 
 module.exports = {
