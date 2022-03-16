ad580ab307dad8ec1628422755761d49320325a5
Sergey Kornienko <madkorney@gmail.com>
2022-03-16 21:28:09 +0300

    feat: add what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -17,45 +17,21 @@ function getSeason(date) {
   const failure = 'Unable to determine the time of year!';
   let season = ''; 
 
-  if (arguments.length == 0) {return failure}
-  if (Object.prototype.toString.call(date) === "[object Date]") {
-    if (isNaN(date)) { 
+  if (arguments.length == 0) {return failure};
+  if (Object.keys(date).length > 0) {throw new Error('Invalid date!')}; // real date has 0
+
+  if (date instanceof Date) {
+    if (isNaN(date) ) { 
       //invalid date
       throw new Error('Invalid date!');
     } else {
-      // date object is valid
       return SEASONSbyMONTHS[date.getMonth()];
     }
   } else {
     // not a date object
-    throw new Error('Invalid date!');
+     throw new Error('Invalid date!');
   }
 }
-
-
-// try {
-// // console.log(getSeason(new Date(2361, 2, 24, 22, 13, 47, 69)));
-// let aaa = () => new Date();
-// console.log(aaa);
-// console.log(getSeason(aaa));
-// // console.log(getSeason('foo'));
-// // console.log(getSeason(['foo']));
-// // console.log(getSeason({john:'smith',}));
-// // console.log(getSeason(new Date('1990-11-28')));
-
-// console.log(getSeason(new Date('1990-13-28')));
-// }  catch(e) {
-//   console.log('поймал ');
-//   console.error(e);
-// }
-
-// () => getSeason('foo'),
-// () => getSeason({ John: 'Smith' }),
-// () => getSeason(20192701),
-// () => getSeason([2019, '27', 0 + '1']),
-// () => getSeason(() => new Date())
-
-
 
 
 module.exports = {
