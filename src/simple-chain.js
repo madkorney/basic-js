30a6d320c8bc242ab377fa8ff0731262a9fb9aa4
Sergey Kornienko <madkorney@gmail.com>
2022-03-17 17:42:54 +0300

    feat: add chain-maker solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chainComplete : false,
+  //linksNumber : 0, 
+  theChain : [], // array of links.  ~~ only add at output
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //throw new NotImplementedError('Not implemented');
+    return this.theChain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    //if (!this.chainComplete) {
+      //this.linksNumber++;
+      let link = '';
+      if (arguments.length == 0) {
+        link = '(  )'; // empty link 
+      } else {
+        link = `( ${value} )`;
+      } 
+      this.theChain.push(link);
+    //}
+
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    const ERROR_MSG = 'You can\'t remove incorrect link!';
+
+    //if (!this.chainComplete) {
+      if (isNaN(position) || (!Number.isInteger(position))) {
+        this.theChain =[];
+        throw new Error(ERROR_MSG)
+      };
+
+
+
+      if (position>0 && position <= this.getLength()) {
+        this.theChain.splice(position - 1, 1);
+      } else {
+        this.theChain =[];
+        throw new Error(ERROR_MSG);
+      }
+
+    //}
+
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //if (!this.chainComplete) {
+      this.theChain.reverse();
+    //}
+
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //this.chainComplete = true;
+    let output = this.theChain.join('~~');
+    this.theChain = [];// todo check?
+
+    return output;
   }
 };
+
+
+
 
 module.exports = {
   chainMaker
