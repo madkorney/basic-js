2847a76600ba92de882c55bcc8c7c0179146d775
Sergey Kornienko <madkorney@gmail.com>
2022-03-18 13:38:45 +0300

    feat: add recursive-depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,11 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+
+  calculateDepth(arr) {
+    let depth = 1;
+      
+    if (arr.filter((element)=> Array.isArray(element)).length == 0) {return depth} // already flat
+    let flattenedArray = arr.flat(1);
+    let haveArrayElements = flattenedArray.filter((element)=> Array.isArray(element)).length != 0;  // after flat(1) - do we have arrays as elements?
+    if (haveArrayElements) {
+      depth = depth + this.calculateDepth(flattenedArray);
+    } else {
+      depth++; 
+      return depth
+    };
+     
+    return depth;
   }
 }
+
+// const calc = new DepthCalculator();
+// console.log(calc.calculateDepth([1, 2, 3, [1], 4, 5, [1]]),'-', 2);
+// console.log(calc.calculateDepth([1, 2, 3, 1, 4, 5, 2]),'-', 1);
+// console.log(calc.calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]),'-', 3);
+// console.log(calc.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]),'-', 4);
+
+
 
 module.exports = {
   DepthCalculator
