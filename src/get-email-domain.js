1ddbc7ee8ee78fb4c3cc3eaa83bb56fc3666cacf
Sergey Kornienko <madkorney@gmail.com>
2022-03-22 00:52:58 +0300

    feat: add get-email-domain solution

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,10 +10,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+ 
+  //validate args
+  if(!email) {return ''}
+
+  let emailDomain = email.substring(email.lastIndexOf('@') + 1);
+
+  return emailDomain
 }
+
+// const tzt = 'pretty@and@simple.example.com';
+// console.log(getEmailDomain(tzt));
 
 module.exports = {
   getEmailDomain
