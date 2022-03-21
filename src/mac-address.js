b46bb145d441e045f3f7ca54a3f9990c9e8f4dd4
Sergey Kornienko <madkorney@gmail.com>
2022-03-22 01:29:59 +0300

    feat: add mac-address solution

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(inputAddress) {
+  const allowedSymbols = '0123456789ABCDEF-'; 
+  //validate args
+   if (!inputAddress) {return false} 
+
+
+   if (inputAddress.split('-').length != 6) {return false} // not 6 groups
+
+   if (!inputAddress.split('-').reduce((previous, current)=>{previous = previous && (current.length===2); return previous},true)) {return false} // does each segment have exactly 2 digits?
+   
+   for (let i = 0; i < inputAddress.length; i++) {
+    if (!allowedSymbols.includes(inputAddress[i].toUpperCase())) {return false} //contains invalid symbols
+   } 
+
+   return true; //it is a MAC address
 }
+
+let mac= 'FF-FF-FF-FF-FF-FF';
+console.log(mac, ' is a MAC? - ', isMAC48Address(mac));
+// assert.strictEqual(isMAC48Address('00-1B-63-84-45-E6'), true);
+// assert.strictEqual(isMAC48Address('Z1-1B-63-84-45-E6'), false);
+// assert.strictEqual(isMAC48Address('not a MAC-48 address'), false);
+// assert.strictEqual(isMAC48Address('FF-FF-FF-FF-FF-FF'), true);
+// assert.strictEqual(isMAC48Address('G0-00-00-00-00-00'), false);
+
 module.exports = {
   isMAC48Address
 };
